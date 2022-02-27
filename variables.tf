4095c56ec09be0665b080c53fb0b1ffc49266e26
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2022-02-27 23:57:27 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
-variable "kozaks" {
-  description = "Скільки козаків пустити вбій(к-сть інстансів)"
+variable "goal_atack" {
+  description = "Дай IP ADARES цих ублюдків:"
+  type        = string
+}
+
+variable "port_atack" {
+  description = "PORT по якому відправити груз:"
   type        = number
-  default     = "1"
+}
+
+variable "kozaks" {
+  description = "Скільки козаків пустити вбій(к-сть інстансів):"
+  type        = number
 }
