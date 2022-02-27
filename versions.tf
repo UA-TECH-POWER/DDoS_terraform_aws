6a414f981d63da2154463997fff9c55c24e6b941
Valerii Tatarin <bugaga1100@gmail.com>
2022-02-27 23:53:07 +0800

    upgrade TF (#1)
    * upgrade TF
    
    * upgrade TF
    
    * upgrade TF

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+  }
+}
