e6c47a2af0faa1dae53430231712e8fb50bf814e
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2022-02-28 03:28:07 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "ssh_command" {
   value = formatlist("ssh ubuntu@%s -i ${abspath(path.root)}/kozak_rsa", aws_instance.ddos.*.public_ip)
 }
+
+#output "work-ok" {
+#  value = formatlist("ssh -oStrictHostKeyChecking=no -i ${abspath(path.root)}/kozak_rsa -fubuntu@%s 'screen -list'", aws_instance.ddos..public_ip)
+#}
